638ccd3ea59ac32171b226a0c26315916e4477c3
Folly Aurelio <folly.aurelio@gmail.com>
2024-05-20 19:30:26 +0200

    Version 1.0

--- Code/Main.py
+++ Code/Main.py
@@ -13,7 +13,6 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("STARFIGHT")
 from Settings import *
 
-os.system("exit")
 pygame.display.set_icon(cut_image(Useful_Item_sprites, 16, 16, 4, 12, 50, 50))
 clock = pygame.time.Clock()
 running = True
